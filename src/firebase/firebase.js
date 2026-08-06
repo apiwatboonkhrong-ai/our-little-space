3b38a67b5f37d5467b1cb4bdc844ad36fbf4a991
Apiwat <apiwat@gmail.com>
2026-08-06 16:31:42 +0700

    Fix Firebase realtime configuration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,16 +4,20 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "คีย์เดิม",
+  apiKey: "AIzaSyAZf9nIjedLerNp-S_hH-M2L7CzAbsHyH0",
+
   authDomain: "our-little-space-7cd8a.firebaseapp.com",
 
   databaseURL:
-    "https://our-little-space-7cd8a-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "https://our-little-space-7cd8a-default-rtdb.asia-southeast1.firebasedatabase.app",
 
   projectId: "our-little-space-7cd8a",
+
   storageBucket:
     "our-little-space-7cd8a.firebasestorage.app",
+
   messagingSenderId: "791325751201",
+
   appId: "1:791325751201:web:ffdb69d35c92ab636cbde0",
 };
 
